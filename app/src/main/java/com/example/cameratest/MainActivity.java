586684615abac8933c99c0e8c008fef5448d13ef
DESKTOP-JDKK7JJ\sun <dtdk7001@gmail.com>
2021-03-08 18:08:04 +0900

    btn 선언

--- app/src/main/java/com/example/cameratest/MainActivity.java
+++ app/src/main/java/com/example/cameratest/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.cameratest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView imgView = findViewById(R.id.imgView);
+
+        findViewById(R.id.btn_picture).setOnClickListener(onclickListener);
+        findViewById(R.id.btn_gallery).setOnClickListener(onclickListener);
     }
+
+    View.OnClickListener onclickListener = v -> {
+      switch (v.getId()){
+          // 촬영
+          case R.id.btn_picture:
+
+          // 이미지 선택
+          case R.id.btn_gallery:
+
+      }
+    };
 }
